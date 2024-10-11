2d7e7d100ec617294777b826e7185beb359de3f7
김동준 <coticoger@Dongjun-MacBook.local>
2024-10-12 00:41:43 +0900

    design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./pages/Header";
 import Explain from "./pages/Explain";
+import Gallery from "./pages/Gallery";
+import Design from "./pages/Design";
 
 const App = () => {
   return(
     <>
       <Header></Header>
       <Explain></Explain>
+      <Gallery></Gallery>
+      <Design></Design>
     </>
   );
 };
