2b4ae964ae405c843d567559b06382df7c17fa73
louisjeon <wjss5115@gmail.com>
2024-10-13 16:20:12 +0900

    change folder structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import Header from "./pages/Header";
-import Explain from "./pages/Explain";
-import Gallery from "./pages/Gallery";
-import Design from "./pages/Design";
+import Header from "./components/Header";
+import Home from "./menu/Home";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
 
 const App = () => {
-  return(
+  return (
     <>
       <Header></Header>
-      <Explain></Explain>
-      <Gallery></Gallery>
-      <Design></Design>
+      <RouterProvider router={router} />
     </>
   );
 };
---
+++ src/menu/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Explain from "../home/Explain";
+import Gallery from "../home/Gallery";
+import Design from "../home/Design";
+
+const Home = () => {
+  return (
+    <>
+      <Explain></Explain>
+      <Gallery></Gallery>
+      <Design></Design>
+    </>
+  );
+};
+
+export default Home;
